99cd8eb80d40f4adf16cdaa548c23e85d3eab4fd
vyder <vyder@lapek>
2016-02-03 23:22:59 +0100

    Init commit gałęzi do rozwijania sieci neuronowej na potrzeby pracy magisterskiej.

---
+++ AI_NeuronNetwork/src/net/wajder/network/NetworkManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.wajder.network;
+
+/**
+ * @author vyder
+ *
+ */
+public class NetworkManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
